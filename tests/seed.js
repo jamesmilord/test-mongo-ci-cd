58fc9def472915a8174578017c84d2a0d420115b
james <jamesonelove5@gmail.com>
2020-04-30 06:46:51 -0400

    added full crud test

--- tests/seed.js
+++ tests/seed.js
@@ -1,19 +1,29 @@
 const User = require("../models/user");
+const uuid = require('uuid')
 
 const seedUsers = [
   {
+    _id: uuid.v4(),
     firstName: "James",
     lastName: "Milord",
     dateOfBirth: "09/06/86"
   },
   {
+    _id: uuid.v4(),
     firstName: "Geraldine",
     lastName: "Prophete",
+    dateOfBirth: "09/06/89"
+  },
+  {
+    _id: 'testid',
+    firstName: "test",
+    lastName: "test2",
     dateOfBirth: "09/06/89"
   }
 ];
 
 const newUser = {
+    _id: uuid.v4(),
     firstName: "Amarah",
     lastName: "Milord",
     dateOfBirth: "06/25/19"
--- tests/user.test.js
+++ tests/user.test.js
@@ -9,23 +9,43 @@ afterAll(async () => {
   await mongoose.connection.close();
 });
 
-describe("GET /users/get", () => {
+describe("GET /users", () => {
     it("should get all users", async () => {
       const res = await request(app)
-        .get("/users/get")
+        .get("/users")
         .expect(200);
       expect(res.body.length).toBe(seedUsers.length);
     });
   });
 
-  describe("POST /users/create", () => {
+  describe("POST /users", () => {
     it("should add new User", async () => {
       const res = await request(app)
-        .post("/users/create")
+        .post("/users")
         .send(newUser)
         .expect(200);
       expect(res.body.firstName).toBe(newUser.firstName);
       expect(res.body.lastName).toBe(newUser.lastName);
       expect(new Date(res.body.dateOfBirth).getTime()).toBe(new Date(newUser.dateOfBirth).getTime());
+    });
+  });
+
+  describe("PATCH /users", () => {
+    it("should patch User", async () => {
+      const res = await request(app)
+        .patch("/users")
+        .send({id: 'testid', data: { firstName: "patched" }})
+        .expect(200);
+      expect(res.body.firstName).toBe("patched");
+    });
+  });
+
+  describe("DELETE /users", () => {
+    it("should delete User", async () => {
+      const res = await request(app)
+        .delete("/users")
+        .send({id: 'testid'})
+        .expect(200);
+      expect(res.body.success).toBe(true);
     });
   });
